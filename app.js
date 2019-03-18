865b37b9c51ce93051ff182312e4185486dd0a37
immanuel.mangihut@hotmail.com <immanuel.mangihut@hotmail.com>
2019-03-18 22:30:28 +0700

    creating controllers

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 const express = require('express');
+const todoController = require('./controllers/todoController');
 
 const app = express();
+const port = 3000;
+
+// set up template engine
+app.set('view engine', 'ejs');
+
+// static files
+app.use(express.static('./public'));
+
+// fire controllers
+todoController(app);
+
+// listen to port
+app.listen(port);
+console.log('Listening to port 3000');
---
+++ controllers/todoController.js
@@ -0,0 +1,11 @@
+module.exports = (app) => {
+  app.get('/todo', (req, res) => {
+
+  });
+  app.post('/todo', (req, res) => {
+
+  });
+  app.delete('/todo', (req, res) => {
+
+  });
+};
