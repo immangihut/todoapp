2095a95cb467ec4290c7a18d2743882b0a145022
immanuel.mangihut@hotmail.com <immanuel.mangihut@hotmail.com>
2019-03-19 21:59:21 +0700

    adding delete feature

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,11 +1,25 @@
+const bodyParser = require('body-parser');
+
+var data = [
+  {item: 'get milk'},
+  {item: 'walk dog'},
+  {item: 'coding'}
+];
+
+const urlencodedParser = bodyParser.urlencoded({extended: false});
+
 module.exports = (app) => {
   app.get('/todo', (req, res) => {
-    res.render('todo');
+    res.render('todo', {todos: data});
   });
-  app.post('/todo', (req, res) => {
-
+  app.post('/todo', urlencodedParser, (req, res) => {
+    data.push(req.body);
+    res.json(data);
   });
-  app.delete('/todo', (req, res) => {
-
+  app.delete('/todo/:item', (req, res) => {
+    data = data.filter((todo) => {
+      return todo.item.replace(/ /g, '-') !== req.params.item;
+    });
+    res.json(data);
   });
 };
